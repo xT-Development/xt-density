3b2bba8072164681704be36f513a936a1634dfb2
xThrasherrr <thrashergamezz@gmail.com>
2024-04-21 13:26:01 -0600

    feat: disable dispatch/car generators

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,3 +1,5 @@
+local config = require 'configs.client'
+
 local lib = lib
 local globalState = GlobalState
 local SetParkedVehicleDensityMultiplierThisFrame =  SetParkedVehicleDensityMultiplierThisFrame
@@ -7,6 +9,17 @@ local SetPedDensityMultiplierThisFrame =            SetPedDensityMultiplierThisF
 local SetScenarioPedDensityMultiplierThisFrame =    SetScenarioPedDensityMultiplierThisFrame
 
 CreateThread(function()
+    for x = 1, #config.removeVehiclesFromGeneratorsInArea do
+        local coords = config.removeVehiclesFromGeneratorsInArea[x]
+        RemoveVehiclesFromGeneratorsInArea((coords.x1 - 300.0), (coords.y1 - 300.0), (coords.z1 - 300.0), (coords.x2 + 300.0), (coords.y2 + 300.0), (coords.z2 + 300.0))
+    end
+
+    if config.disableDispatchServices then
+        for i = 1, 15 do
+            EnableDispatchService(i, false)
+        end
+    end
+
     while true do
         local parkedDensity =       globalState.disableDensity and 0 or globalState.density['parked']
         local vehicleDensity =      globalState.disableDensity and 0 or globalState.density['vehicle']
---
+++ configs/client.lua
@@ -0,0 +1,13 @@
+return {
+    disableDispatchServices = true,                 -- Disable emergency service AIs from responding to downed NPCs
+    removeVehiclesFromGeneratorsInArea = {          -- Disable car generators in areas
+        { x1 = 335.2616, y1 = -1432.455, z1 = 46.51, x2 = 335.2616, y2 = -1432.455, z2 = 46.51 },       -- Central LS Medical Center
+        { x1 = 441.8465, y1 = -987.99, z1 = 30.68, x2 = 441.8465, y2 = -987.99, z2 = 30.68 },           -- MRPD
+        { x1 = 316.79, y1 = -592.36, z1 = 43.28, x2 = 316.79, y2 = -592.36, z2 = 43.28 },               -- Pillbox
+        { x1 = -2150.44, y1 = 3075.99, z1 = 32.8, x2 = -2150.44, y2 = 3075.99, z2 = 32.8 },             -- Military
+        { x1 = -1108.35, y1 = 4920.64, z1 = 217.2, x2 = -1108.35, y2 = 4920.64, z2 = 217.2 },           -- Nudist Colony
+        { x1 = -458.24, y1 = 6019.81, z1 = 31.34, x2 = -458.24, y2 = 6019.81, z2 = 31.34 },             -- Paleto PD
+        { x1 = 1854.82, y1 = 3679.4, z1 = 33.82, x2 = 1854.82, y2 = 3679.4, z2 = 33.82 },               -- Sandy PD
+        { x1 = -724.46, y1 = -1444.03, z1 = 5.0, x2 = -724.46, y2 = -1444.03, z2 = 5.0 },               -- Remove Choppers
+    }
+}
